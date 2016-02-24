15b2fbada0ef7208da2375c7fb156eb0571a92f0
Douglas Q. dos Santos <douglas.q.santos@gmail.com>
2016-02-24 15:15:25 -0300

    Added more some new themes, added support to pt-BR, used only the common language more: dns vim dos dockerfile erlang pf accesslog erb yaml powershell lua scss

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * english language file for syntaxhighlightjs plugin
+ *
+ * @author Claud D. Park <posquit0.bj@gmail.com>
+ */
+
+// keys need to match the config setting name
+$lang['syntax'] = 'Qual sintaxe deve ser utilizada para ser aplicada as configurações do syntaxhighlightjs?';
+$lang['theme'] = 'Qual Tema deve ser utilizado para o syntaxhighlightjs?';
+$lang['restrictedClasses'] = 'Limitar o uso do plugin somente para as seguintes classes (separadas por vírgula)';
+
+
+//Setup VIM: ex: et ts=4 :
